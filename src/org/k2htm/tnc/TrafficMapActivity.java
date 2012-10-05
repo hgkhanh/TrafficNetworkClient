59140557db0ceef8df9a4694afb57e25da617fab
hgkhanh <hkhanh.ng@gmail.com>
2012-10-05 23:47:17 +0700

    add report button

--- src/org/k2htm/tnc/TrafficMapActivity.java
+++ src/org/k2htm/tnc/TrafficMapActivity.java
@@ -42,19 +42,28 @@ public class TrafficMapActivity extends MapActivity implements LocationListener 
 		getLastLocation();
 		animateToCurrentLocation();
 		drawCurrPositionOverlay();
+		drawMalls();
 	}
 
 	public void getLastLocation() {
 		String provider = getBestProvider();
 		currentLocation = locationManager.getLastKnownLocation(provider);
+		/*
+		 * The next 4 lines are used to hardcode our location If you wish to get
+		 * your current location remember to comment or remove them
+		 */
+		currentPoint = new GeoPoint(29647929, -82352486);
+		currentLocation = new Location("");
+		currentLocation.setLatitude(currentPoint.getLatitudeE6() / 1e6);
+		currentLocation.setLongitude(currentPoint.getLongitudeE6() / 1e6);
 		if (currentLocation != null) {
 			setCurrentLocation(currentLocation);
-			((TextView) findViewById(R.id.providerText)).setText("Provider :"
-					+ getBestProvider());
 		} else {
 			Toast.makeText(this, "Location not yet acquired", Toast.LENGTH_LONG)
 					.show();
 		}
+		((TextView) findViewById(R.id.providerText)).setText("Provider :"
+				+ getBestProvider());
 	}
 
 	public void animateToCurrentLocation() {
@@ -74,18 +83,32 @@ public class TrafficMapActivity extends MapActivity implements LocationListener 
 	}
 
 	public void setCurrentLocation(Location location) {
-		int currLatitude = (int) (location.getLatitude() * 1E6);
-		int currLongitude = (int) (location.getLongitude() * 1E6);
-		currentPoint = new GeoPoint(currLatitude, currLongitude);
+		/*
+		 * int currLatitude = (int) (location.getLatitude()*1E6); int
+		 * currLongitude = (int) (location.getLongitude()*1E6); currentPoint =
+		 * new GeoPoint(currLatitude,currLongitude);
+		 */
+		/*
+		 * ======================================================================
+		 * ================== /*The Above Code displays your correct current
+		 * location, but for the sake of the demo I will be hard coding your
+		 * current location to the University of Florida, to get your real
+		 * current location, comment or delete the line of code below and
+		 * uncomment the code above.
+		 */
+
+		currentPoint = new GeoPoint(29647929, -82352486);
 		currentLocation = new Location("");
-		currentLocation.setLatitude(currentPoint.getLatitudeE6() / 1E6);
-		currentLocation.setLongitude(currentPoint.getLongitudeE6() / 1E6);
+		currentLocation.setLatitude(currentPoint.getLatitudeE6() / 1e6);
+		currentLocation.setLongitude(currentPoint.getLongitudeE6() / 1e6);
+
 		((TextView) findViewById(R.id.latitudeText)).setText("Latitude : "
-				+ String.format("%.6f", (location.getLatitude())));
+				+ String.valueOf((int) (currentLocation.getLatitude() * 1E6)));
 		((TextView) findViewById(R.id.longitudeText)).setText("Longitude : "
-				+ String.format("%.6f", (location.getLongitude())));
+				+ String.valueOf((int) (currentLocation.getLongitude() * 1E6)));
 		((TextView) findViewById(R.id.accuracyText)).setText("Accuracy : "
 				+ String.valueOf(location.getAccuracy()) + " m");
+		drawCurrPositionOverlay();
 	}
 
 	public void centerToCurrentLocation(View view) {
@@ -106,6 +129,37 @@ public class TrafficMapActivity extends MapActivity implements LocationListener 
 		}
 	}
 
+	
+	public void drawMalls(){
+	    Drawable marker = getResources().getDrawable(R.drawable.incidents);
+	    TrafficOverlay mallsPos = new TrafficOverlay(marker,mapView);
+	        GeoPoint[] mallCoords = new GeoPoint[6];
+	        //Load Some Random Coordinates in Miami, FL
+	        mallCoords[0] = new GeoPoint(29656582,-82411151);//The Oaks Mall
+	        mallCoords[1] = new GeoPoint(29649831,-82376347);//Creekside mall
+	        mallCoords[2] = new GeoPoint(29674146,-8238905);//Millhopper Shopping Center
+	        mallCoords[3] = new GeoPoint(29675078,-82322617);//Northside Shopping Center
+	        mallCoords[4] = new GeoPoint(29677017,-82339761);//Gainesville Mall
+	        mallCoords[5] = new GeoPoint(29663835,-82325599);//Gainesville Shopping Center
+	        List<Overlay> overlays = mapView.getOverlays();
+	    OverlayItem overlayItem = new OverlayItem(mallCoords[0], "The Oaks Mall", "6419 W Newberry Rd, Gainesville, FL 32605");
+	    mallsPos.addOverlay(overlayItem);
+	    overlayItem = new OverlayItem(mallCoords[1], "Creekside Mall", "3501 Southwest 2nd Avenue, Gainesville, FL");
+	    mallsPos.addOverlay(overlayItem);
+	    overlayItem = new OverlayItem(mallCoords[2], "Millhopper Shopping Center", "NW 43rd St & NW 16th Blvd. Gainesville, FL");
+	    mallsPos.addOverlay(overlayItem);
+	    overlayItem = new OverlayItem(mallCoords[3], "Northside Shopping Center", "Gainesville, FL");
+	    mallsPos.addOverlay(overlayItem);
+	    overlayItem = new OverlayItem(mallCoords[4], "Gainesville Mall", "2624 Northwest 13th Street Gainesville, FL 32609-2834");
+	    mallsPos.addOverlay(overlayItem);
+	    overlayItem = new OverlayItem(mallCoords[5], "Gainesville Shopping Center", "1344 N Main St Gainesville, Florida 32601");
+	    mallsPos.addOverlay(overlayItem);
+	    overlays.add(mallsPos);
+	    mallsPos.setCurrentLocation(currentLocation);
+	}
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_traffic_map, menu);
--- src/org/k2htm/tnc/TrafficOverlay.java
+++ src/org/k2htm/tnc/TrafficOverlay.java
@@ -1,12 +1,15 @@
 package org.k2htm.tnc;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 import com.readystatesoftware.mapviewballoons.BalloonItemizedOverlay;
@@ -46,8 +49,31 @@ public class TrafficOverlay extends BalloonItemizedOverlay<OverlayItem> {
 
 	@Override
 	protected boolean onBalloonTap(int index, OverlayItem item) {
-		Toast.makeText(mContext, "Overlay Item " + index + " tapped!",
-				Toast.LENGTH_LONG).show();
+		String tmp = incidents.get(index).getTitle();
+		GeoPoint mallPoint = incidents.get(index).getPoint();
+		Location tmpLoc = convertGpToLoc(mallPoint);
+		double distance = ((currentLocation).distanceTo(tmpLoc)) * (0.001);
+		DecimalFormat df = new DecimalFormat("#.##");
+		tmp = tmp + " is " + String.valueOf(df.format(distance)) + " km away.";
+		Toast.makeText(mContext, tmp, Toast.LENGTH_LONG).show();
 		return true;
+	}
+
+	public Location convertGpToLoc(GeoPoint gp) {
+		Location convertedLocation = new Location("");
+		convertedLocation.setLatitude(gp.getLatitudeE6() / 1e6);
+		convertedLocation.setLongitude(gp.getLongitudeE6() / 1e6);
+		return convertedLocation;
+	}
+
+	@Override
+	public boolean onTap(GeoPoint p, MapView mapView) {
+		// TODO Auto-generated method stub
+		Toast.makeText(
+				mContext,
+				"long : " + p.getLongitudeE6() + "  | lat : "
+						+ p.getLatitudeE6(), Toast.LENGTH_SHORT).show();
+
+		return super.onTap(p, mapView);
 	}
 }
